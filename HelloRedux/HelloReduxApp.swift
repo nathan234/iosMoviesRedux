845e5f43b3fb2625c923a46319c83e428c6dfe3f
Nathan Retta <nretta@gmail.com>
2021-03-04 22:37:26 -0700

    Initial Commit

---
+++ HelloRedux/HelloReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloReduxApp.swift
+//  HelloRedux
+//
+//  Created by Nathan Retta on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
